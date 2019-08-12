f3ff7c292dd72c4a7616c42075390550834ff0ea
dolphin9 <dolphin9@foxmail.com>
2019-08-12 16:18:00 +0800

    ###

--- dataset/pick.py
+++ dataset/pick.py
@@ -26,25 +26,25 @@ kitefiles = os.listdir(kitedir)
 #    else:
 #       os.rename(traindir+'/'+file,txtdir+'/'+file)
        
-for jpgfile in jpgfiles:
-        for txtfile in txtfiles:
-                if jpgfile[:-4] == txtfile[:-4]:
-                      os.rename(jpgdir+'/'+jpgfile,traindir+'/'+jpgfile)
-                      os.rename(txtdir+'/'+txtfile,traindir+'/'+txtfile)
-                      continue
-        pass
-
-#for txtfile in trainfiles:
-#        for jpgfile in jpgfiles:
+#for jpgfile in jpgfiles:
+#        for txtfile in txtfiles:
 #                if jpgfile[:-4] == txtfile[:-4]:
-#                        if txtfile[-3:] == 'txt':
-#                                filename1 = traindir+ '/' + txtfile
-                                #print(filename1)
-#                                filename2 = jpgdir + '/' + txtfile
-                                #print(filename2)
-#                                os.system('cp %s %s' % (filename1, filename2)) 
-#                                if os.path.isfile(filename2):
-#                                       print ('copy file success')
+#                      os.rename(jpgdir+'/'+jpgfile,traindir+'/'+jpgfile)
+#                      os.rename(txtdir+'/'+txtfile,traindir+'/'+txtfile)
+#                      continue
+#        pass
+
+for txtfile in trainfiles:
+    if txtfile[-3:] = 'txt':
+        jpgfile = txtfile[:-4] + '.jpg'
+        if jpgfile in kitefiles:
+            filename1 = traindir+ '/' + txtfile
+            #print(filename1)
+            filename2 = kitedir + '/' + txtfile
+            #print(filename2)
+            os.system('cp %s %s' % (filename1, filename2)) 
+            if os.path.isfile(filename2):
+            print ('copy file success')
                                         
 #for jpgfile in kitefiles:
 #        txtfile = jpgfile[:-4] + '.txt'
