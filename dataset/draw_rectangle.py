9ad05ae37b63e0f038a99b65c7db8d0841eaee78
dolphin9 <dolphin9@foxmail.com>
2019-09-03 23:19:46 +0800

    Revise draw_rectangle.py && my_annotation.py.

--- dataset/draw_rectangle.py
+++ dataset/draw_rectangle.py
@@ -1,11 +1,17 @@
 import xml.etree.ElementTree as ET
 import cv2
+import os
 
 classes = ["kite", "balloon","bird-nest"]
 
-def draw(name):
-    img_file = "image/%s.jpg" % name
-    lbl_file = "voc_new/%s.xml" % name
+wd = os.getcwd()
+bboxfilepath = os.path.join(wd, 'labels') 
+imagefilepath = os.path.join(wd, 'images') 
+imagesavepath = os.path.join(wd, 'rects') 
+
+def draw_xml(name):
+    img_file = "%s/%s.jpg" % (imagefilepath, name)
+    lbl_file = "%s/%s.xml" % (bboxfilepath, name)
     img = cv2.imread(img_file)
     # h, w, _ = img.shape
     # print(h, w)
@@ -24,7 +30,39 @@ def draw(name):
             xmin, ymin, xmax, ymax = (int(xmlbox.find('xmin').text), int(xmlbox.find('ymin').text), int(xmlbox.find('xmax').text), int(xmlbox.find('ymax').text))
             img = cv2.rectangle(img, (xmin, ymin), (xmax, ymax), (1, 1, 0, 0), 2)
 
-    cv2.imwrite("rects/%s.jpg" % name, img)
+    cv2.imwrite("%s/%s.jpg" % (imagesavepath, name), img)
+
+def draw_txt(name):
+    img_file = "%s/%s.jpg" % (imagefilepath, name)
+    lbl_file = "%s/%s.txt" % (bboxfilepath, name)
+    img = cv2.imread(img_file)
+    h, w, _ = img.shape
+    # print(h, w)
+    with open(lbl_file, "r") as f:
+        for line in f.readlines():
+            # xmin, ymin, xmax, ymax = map(float, line.split(' ')[1:])
+            # yctr, xctr, ywid, xwid = map(float, line.split(' ')[1:])
+            xctr, yctr, xwid, ywid = map(float, line.split(' ')[1:])
+            # ywid, xwid, yctr, xctr = map(float, line.split(' ')[1:])
+            ymin = yctr - ywid/2
+            ymax = yctr + ywid/2
+            xmin = xctr - xwid/2
+            xmax = xctr + xwid/2
+            # print((xmin, ymin), (xmax, ymax))
+
+            xmin = int(xmin * w)
+            ymin = int(ymin * h)
+            xmax = int(xmax * w)
+            ymax = int(ymax * h)
+            img = cv2.rectangle(img, (xmin, ymin), (xmax, ymax), (1, 1, 0, 0), 2)
+
+    cv2.imwrite("%s/%s.jpg" % (imagesavepath, name), img)
+
+def draw(name):
+    if os.path.isfile('%s/%s.xml'%(bboxfilepath, name)):
+        draw_xml(name)
+    else:
+        draw_txt(name)
 
 with open("main/train.txt", "r") as f:
     for name in f.readlines():
---
+++ dataset/my_annotation.py
@@ -0,0 +1,68 @@
+import xml.etree.ElementTree as ET
+import cv2
+import os
+
+sets=['train', 'val', 'test']
+
+classes = ["kite","balloon","bird-nest"]
+
+wd = os.getcwd()
+bboxfilepath = os.path.join(wd, 'labels') 
+imagefilepath = os.path.join(wd, 'images') 
+
+
+def convert_annotation_xml(image_id, list_file):
+    in_file = open('%s/%s.xml'%(bboxfilepath, image_id))
+    tree=ET.parse(in_file)
+    root = tree.getroot()
+
+    for obj in root.iter('object'):
+        difficult = obj.find('difficult').text
+        cls = obj.find('name').text
+        if cls not in classes or int(difficult)==1:
+            continue
+        cls_id = classes.index(cls)
+        xmlbox = obj.find('bndbox')
+        b = (int(xmlbox.find('xmin').text), int(xmlbox.find('ymin').text), int(xmlbox.find('xmax').text), int(xmlbox.find('ymax').text))
+        list_file.write(" " + ",".join([str(a) for a in b]) + ',' + str(cls_id))
+
+
+def convert_annotation_txt(image_id, list_file):
+    in_file = open('%s/%s.txt'%(bboxfilepath, image_id))
+    in_image= cv2.imread('%s/%s.jpg'%(imagefilepath, image_id))
+    height, width, _ = in_image.shape
+
+    for line in in_file.readlines():
+        s = line.split(" ")
+        cls_id = int(s[0])
+        xctr, yctr, xwid, ywid = map(float, s[1:])
+        ymin = yctr - ywid/2
+        ymax = yctr + ywid/2
+        xmin = xctr - xwid/2
+        xmax = xctr + xwid/2
+
+        xmin = int(xmin * width)
+        ymin = int(ymin * height)
+        xmax = int(xmax * width)
+        ymax = int(ymax * height)
+
+        bbox = [xmin, ymin, xmax, ymax]
+        list_file.write(" " + ",".join([str(a) for a in bbox]) + ',' + str(cls_id))
+
+
+def convert_annotation(image_id, list_file):
+    if os.path.isfile('%s/%s.xml'%(bboxfilepath, image_id)):
+        convert_annotation_xml(image_id, list_file)
+    else:
+        convert_annotation_txt(image_id, list_file)
+
+
+for image_set in sets:
+    image_ids = open('main/%s.txt'%(image_set)).read().strip().split()
+    list_file = open('%s.txt'%(image_set), 'w')
+    for image_id in image_ids:
+        list_file.write('%s/%s.jpg'%(imagefilepath, image_id))
+        convert_annotation(image_id, list_file)
+        list_file.write('\n')
+    list_file.close()
+
